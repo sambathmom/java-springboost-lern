9651d8cb276c0b0688714e1a279f86659b5237dc
sambathmom <sambathmom06@gmail.com>
2020-09-23 13:31:41 +0700

    first commit

---
+++ src/test/java/java/springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package java.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
